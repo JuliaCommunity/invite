a0bc9ed2b8d05e064df878454d7142aa0d6c4d6f
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-12-15 22:28:54 -0600

    Add initial version of the invite to GitHub bot for the Julia Community

---
+++ invite.jl
@@ -0,0 +1,29 @@
+using GitHub
+using AirTable
+
+AIR_TABLE_KEY = ARGS[1]
+AIR_TABLE_BASE = ARGS[2]
+AIR_TABLE_NAME = ARGS[3]
+
+AUTH_TOKEN = ARGS[4]
+
+key = Airtable.Credential(AIR_TABLE_KEY)
+
+tab = AirTable(AIR_TABLE_NAME, AirBase(AIR_TABLE_BASE))
+
+myauth = GitHub.authenticate(AUTH_TOKEN)
+
+for row in Airtable.query(tab)
+    
+    # Invite people to the JuliaCommunity GitHub org, which has the ID = 6883862 (only visible via the API)
+    try 
+        response = invitations(auth=myauth, "JuliaCommunity", Dict("email"=>row[:Email],"role"=>"direct_member","team_ids"=>[6883862]))
+    catch
+        print("Failed to send invite, deleting record from AirTable")
+        Airtable.delete!(row)
+    end
+
+    # Assuming successful, we need to remove the row
+    Airtable.delete!(row)
+    break
+end
